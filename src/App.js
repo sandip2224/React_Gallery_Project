0bde1660c88e14d72ebe7b6d992886c9029906ea
sandip2224 <sandipan2224@gmail.com>
2022-01-27 16:12:29 +0530

    Understanding and accessing form events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import SearchBar from './components/SearchBar'
 
-function App() {
-	return (
-		<div className="App ui container" style={{ marginTop: '10px' }}>
-			<SearchBar />
-		</div>
-	);
+class App extends React.Component {
+
+	onFormSubmit = (val) => {
+		console.log(val)
+	}
+
+	render() {
+		return (
+			<div className="App ui container" style={{ marginTop: '10px' }}>
+				<SearchBar onFormSubmit={this.onFormSubmit} />
+			</div >
+		);
+	}
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,27 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+
+    state = { term: '' };
+
+    handleChange = (e) => {
+        this.setState({ term: e.target.value })
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        this.props.onFormSubmit(this.state.term)
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.handleFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input type="text" value={this.state.term} onChange={this.handleChange} />
+                        <p>{this.state.term}</p>
                     </div>
-
                 </form>
             </div>
         )
